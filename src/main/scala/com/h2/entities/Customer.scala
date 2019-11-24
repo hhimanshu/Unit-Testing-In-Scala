8d00f4f4354058f19bed30614871a4bba55093ce
hhimanshu <harit.himanshu@yahoo.in>
2019-11-24 15:53:58 -0800

    Proof of a bug

--- src/main/scala/com/h2/entities/Customer.scala
+++ src/main/scala/com/h2/entities/Customer.scala
@@ -8,3 +8,8 @@ class Customer(val first: String, val last: String,
   val id: UUID = UUID.randomUUID()
   override def toString: String = s"$id -> $first,$last,$email"
 }
+
+/** M2 - Example of customer creation exception because of invalid date */
+object Run extends App {
+  val Jim = new Customer("Jim", "Ben", Email("jim", "ben.com"), LocalDate.parse("a/b/c"))
+}
