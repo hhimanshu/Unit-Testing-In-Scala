30cf8c0802daab7ba90305472fe8e709a9585a82
hhimanshu <harit.himanshu@yahoo.in>
2019-12-14 11:10:53 -0800

    module 05: Added Matchers tests for Emptiness

---
+++ src/test/scala/matchers/EmptinessSpec.scala
@@ -0,0 +1,25 @@
+package matchers
+
+import com.h2.services.{Currency, CustomerService}
+
+class EmptinessSpec extends UnitSpec {
+  val customerService: CustomerService = new CustomerService {}
+
+  behavior of "Customer for emptiness"
+
+  it should "return empty for customer's last name" in {
+    val (first, last, email, dateOfBirth) = ("Shane", "", "shane@google.com", "1983/02/01")
+    val customerId = customerService.createNewCustomer(first, last, email, dateOfBirth)
+    val customer = customerService.getCustomer(customerId).get
+
+    customer.last should be (empty)
+    customer.last shouldBe empty
+  }
+
+  behavior of "Currencies inside wallet"
+  it should "be empty when no currencies are added to wallet" in {
+    val wallet: List[Currency] = List.empty
+
+    wallet should be (empty)
+  }
+}
