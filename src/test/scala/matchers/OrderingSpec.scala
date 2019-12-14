946a7f21c301bbbffe7b902e7de8764cda284f8b
hhimanshu <harit.himanshu@yahoo.in>
2019-12-14 06:36:39 -0800

    module 05: Added Matchers tests for Ordering

---
+++ src/test/scala/matchers/OrderingSpec.scala
@@ -0,0 +1,35 @@
+package matchers
+
+import com.h2.services.Currency
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class OrderingSpec extends AnyFlatSpec with Matchers {
+  behavior of "Currency Conversion Cost in Comparison"
+
+  it should "report equal costs for 10 USD, 10 USD" in {
+    val tenUsd: Currency = "10 USD"
+    val anotherTenUsd: Currency = "10 USD"
+
+    tenUsd.costInDollars.amount should be >= anotherTenUsd.costInDollars.amount
+  }
+
+  it should "report higher costs for 100 USD, 10 USD" in {
+    val tenUsd: Currency = "10 USD"
+    val hundredUsd: Currency = "100 USD"
+
+    hundredUsd.costInDollars.amount should be > tenUsd.costInDollars.amount
+  }
+
+  it should "report 1 USD < 10 USD" in {
+    val oneUsd: Currency = "1 USD"
+    val tenUsd: Currency = "10 USD"
+
+    oneUsd.costInDollars.amount should be < tenUsd.costInDollars.amount
+  }
+
+  it should "report NZD < USD " in {
+    "NZD" should be < "USD"
+  }
+
+}
