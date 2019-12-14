f38da38aefbee34c9c6b04d8d3e87182ef502d19
hhimanshu <harit.himanshu@yahoo.in>
2019-12-14 08:20:09 -0800

    module 05: Added Matchers tests for Container

---
+++ src/test/scala/matchers/ContainerSpec.scala
@@ -0,0 +1,66 @@
+package matchers
+
+import com.h2.services.Currency
+
+class ContainerSpec extends UnitSpec {
+
+  behavior of "Currencies in a wallet"
+
+  it should "contain a currency that is added to a List wallet" in  {
+    val oneUsd: Currency = "1 USD"
+    val twoEuros: Currency = "2 EUR"
+    val tenCad: Currency = "10 CAD"
+
+    val wallet = List(oneUsd, twoEuros, tenCad)
+
+    wallet should contain (oneUsd)
+  }
+
+  it should "contain a currency that is added to a Set wallet" in  {
+    val oneUsd: Currency = "1 USD"
+    val twoEuros: Currency = "2 EUR"
+    val tenCad: Currency = "10 CAD"
+
+    val wallet = Set(oneUsd, twoEuros, tenCad)
+
+    wallet should contain (oneUsd)
+  }
+
+  it should "contain a currency that is added to a Map wallet" in  {
+    val oneUsd: Currency = "1 USD"
+    val twoEuros: Currency = "2 EUR"
+    val tenCad: Currency = "10 CAD"
+
+    val wallet: Map[String, Currency] = Map(
+      "USD" -> oneUsd,
+      "EUR" -> twoEuros,
+      "CAD" -> tenCad
+    )
+    wallet should contain ("USD" -> oneUsd)
+  }
+
+  it should "contain a oneOf 1 USD that is added to Set wallet" in  {
+    val oneUsd: Currency = "1 USD"
+    val twoEuros: Currency = "2 EUR"
+    val tenCad: Currency = "10 CAD"
+    val hundredInr: Currency = "100 INR"
+
+    val wallet: Set[Currency] = Set(oneUsd, oneUsd, twoEuros, twoEuros, tenCad, tenCad)
+
+    wallet should contain oneOf (oneUsd, hundredInr)
+    wallet should contain oneElementOf  List(oneUsd, hundredInr)
+  }
+
+  it should "contain a noneOf 100 INR that is added to Set wallet" in  {
+    val oneUsd: Currency = "1 USD"
+    val twoEuros: Currency = "2 EUR"
+    val tenCad: Currency = "10 CAD"
+    val hundredInr: Currency = "100 INR"
+
+    val wallet: Set[Currency] = Set(oneUsd, oneUsd, twoEuros, twoEuros)
+    wallet should contain noneOf (tenCad, hundredInr)
+    // wallet should contain noneOf (tenCad, oneUsd) // this would fail since oneUsd is available in wallet
+    wallet should contain noElementsOf List(tenCad, hundredInr)
+
+  }
+}
