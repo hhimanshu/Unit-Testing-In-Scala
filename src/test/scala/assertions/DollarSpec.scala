71fdd3acf885e0a0ffe30c55827e5614958dbf3c
William Burdett <williamburdett@williams-air.mynet>
2022-05-13 11:35:26 +0100

    clues added to tests

--- src/test/scala/assertions/DollarSpec.scala
+++ src/test/scala/assertions/DollarSpec.scala
@@ -27,4 +27,47 @@ class DollarSpec extends AnyFlatSpec{
     // this allowed us to use the comparison symbols '<' and '>'
     assert(twoDollars < tenDollars)
   }
+
+  it should "correctly add two Dollar amounts" in {
+    val tenDollars = Dollars(10)
+    val twoDollars = Dollars(2)
+    assertResult("$12"){
+      (twoDollars + tenDollars).toString
+    }
+  }
+
+  it should "correctly subtract two Dollar amounts" in {
+    val tenDollars = Dollars(10)
+    val twoDollars = Dollars(2)
+    assertResult("$8"){
+      (tenDollars - twoDollars).toString
+    }
+  }
+
+  it should "correctly identify that $4 == $4" in {
+    val fourDollars = Dollars(4)
+    assertResult(true){
+      fourDollars === fourDollars
+    }
+  }
+
+  it should "throw an exception when an invalid integer is provided to create Dollars" in {
+    // the type of error expected in []
+    assertThrows[ArithmeticException]{
+      // the input you expect to cause an exception to be thrown
+      Dollars(10/0)
+    }
+  }
+
+  it should "have every dollar more than 0" in {
+    // assume api call
+    val dollars: List[Dollars] = List.empty
+
+    // PreConditions: The test would pass without this line. We want to cancel it as there are no dollars.
+    assume(dollars.nonEmpty)
+
+    dollars.foreach{ d =>
+      assert(d.amount > 0)
+    }
+  }
 }
