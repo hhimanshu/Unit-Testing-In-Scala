a850a8a6fef9f76c742575c7df62430d81a841be
William Burdett <williamburdett@williams-air.mynet>
2022-05-17 11:44:01 +0100

    emptiness, logical, exception spec added

---
+++ src/test/scala/matchers/LogicalSpec.scala
@@ -0,0 +1,23 @@
+package matchers
+
+import com.h2.services.Currency
+
+// COMBINE LOGICAL OPERATORS FOR MULTIPLE BOOLEANS
+
+class LogicalSpec extends UnitSpec {
+
+  behavior of "Currencies as logical and/or"
+
+  it should "successfully match logical expression with 'and' condition for a currency" in {
+    val tenNzd: Currency = "10 NZD"
+
+    tenNzd.costInDollars.amount should (be > (0) and be <= (10))
+  }
+
+  it should "successfully match logical expression with 'OR' condition for a currency" in {
+    val tenNzd: Currency = "10 NZD"
+
+    tenNzd.code should (have length (0) or equal ("NZD"))
+  }
+
+}
